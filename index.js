c7bfe8ed9101de173399673acb0c9ae435b0b842
Johannes Andersen <johanes1@online.no>
2016-02-01 12:44:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var HTTPBasicStrat = require('passport-http').BasicStrategy;
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'johannes',
+    database : 'cars'
+});
+
+connection.connect();
+
+passport.use(new HTTPBasicStrat({}, function(username, password, cb){
+    console.log("Authenticating");
+    connection.query("SELECT * from Users WHERE name=?", username, function(err, rows){
+        if (err) { return cb(err);}
+        if (rows.length > 0){
+            return cb(null, rows[0]);
+        }else {
+            return cb(null, false);
+        }
+    });
+}));
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+app.use((req, res, next)=>{
+    req.body.user = {name: "Per"};
+    next();
+});
+
+var port = process.env.port || 8080;
+
+var router = express.Router();
+
+function ensureAuthenticated(req, res, next){
+    if (req.isAuthenticated()){
+        next();
+    }else{
+        next();
+        //res.send(403);
+    }
+}
+
+
+app.use("/api", passport.authenticate('basic', {session: false}));
+
+router.get("/", function(req, res){
+    console.log("Getting /api");
+    res.json({Hello: req.user.name});
+});
+
+app.use("/api", router);
+
+app.listen(port);
+console.log("Magic is due on port "+port);
