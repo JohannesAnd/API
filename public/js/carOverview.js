e12180750efa9a8db3b85e2dca27b93a9e23752b
Johannes Andersen <johanes1@online.no>
2016-04-07 21:09:00 +0200

    Real-time position on google maps working

--- public/js/carOverview.js
+++ public/js/carOverview.js
@@ -1,4 +1,16 @@
+var markers = {};
 $(document).ready(function() {
+    var mapCenter = new google.maps.LatLng(59.340458, 18.057340);
+    var mapCanvas = document.getElementById("map_canvas");
+    var mapOptions = {
+        center: mapCenter,
+        zoom: 13,
+        scrollwheel: true,
+        draggable: true,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
     function updateCars(){
         $.ajax({
             url: "/organizations/" + id + "/carOverview/getData",
@@ -7,20 +19,44 @@ $(document).ready(function() {
             data: {},
             success: function (data) {
                 $("#active").html(data.cars.map(function (car) {
-                        return (
-                            "<div class='carContainer'>" +
-                                "<h1>" + car.registration + "</h1>" +
-                                "<h2>" + car.registration_time + "</h2>" +
-                                "<h2>" + car.trip_id + "</h2>" +
-                                "<h2>" + car.longitude + "</h2>" +
-                                "<h2>" + car.latitude + "</h2>" +
-                                "<h2>" + car.active + "</h2>" +
-                            "</div>"
-                        );
-                    }));
+                    if (markers[car.registration]) {
+                        markers[car.registration].setPosition(new google.maps.LatLng(car.latitude, car.longitude));
+                    } else {
+                        markers[car.registration] = new google.maps.Marker({
+                            position: new google.maps.LatLng(car.latitude, car.longitude),
+                            map: map
+                        });
+                    }
+                    return (
+                        "<div class='carContainer'>" +
+                            "<h1>" + car.registration + "</h1>" +
+                            "<h2>" + car.registration_time + "</h2>" +
+                            "<h2>" + car.trip_id + "</h2>" +
+                            "<h2>" + car.longitude + "</h2>" +
+                            "<h2>" + car.latitude + "</h2>" +
+                            "<h2>" + car.active + "</h2>" +
+                        "</div>"
+                    );
+                }));
             }
         });
     }
+    var bittersMap = (function () {
+        return {
+            init: function () {
+                map.set("styles", [{
+                    featureType: "landscape",
+                    elementType: "geometry",
+                    stylers: [
+                        { hue: "#ffff00" },
+                        { saturation: 30 },
+                        { lightness: 10}
+                    ]}
+                ]);
+            }
+        };
+    }());
+    bittersMap.init();
     updateCars();
     setInterval(updateCars, 1000);
 });
